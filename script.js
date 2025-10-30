f2bfa90239048e9167697bafae80824d8a234c6c
eshaanand2 <eshaanand35@gmail.com>
2025-10-30 18:39:07 +0000

    classwork

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-// add javascript here
+// global variables
+let level, answer, score;
+const levelArr = document.getElementsByName("level");
+const scoreArr = [];
+date.textContent = time();
+
+// event listeners
+playBtn.addEventListener("click",play);
+guessBtn.addEventListener("click",makeGuess);
+
+function play(){
+
+}
+function makeGuess(){
+
+}
+function reset(){
+
+}
+function updateScore(){
+    scoreArr.push(score);
+    scoreArr.sort((a,b)=>a-b);
+    let lb=document.getElementsByName("leaderboard");
+
+    wins.textContent="Total wins: "+scoreArr.length;
+    let sum = 0;
+    for(let i=0;i<scoreArr.length;i++){
+        sum+=scoreArr[i];
+        if(i<lb.length){
+            lb[i].textContent=scoreArr[i];
+        }
+    }
+    let avg=sum/scoreArr.length;
+    avgScore.textContent="Average Score: "+avg.toFixed(2);
+}
+function time(){
+    let d=new Date();
+    // concatenate a string with all the date info
+    return d;
+}
