3e40517c7e70d41026d3ae2511c3a09753f0d0a3
eshaanand2 <eshaanand35@gmail.com>
2025-10-31 04:16:24 +0000

    cw

--- script.js
+++ script.js
@@ -9,14 +9,52 @@ playBtn.addEventListener("click",play);
 guessBtn.addEventListener("click",makeGuess);
 
 function play(){
-
+    score = 0; // sets score to 0 every new game
+    playBtn.disabled = true;
+    guessBtn.disabled = false;
+    guess.disabled = false;
+    for(let i=0; i<levelArr.length; i++){
+        if(levelArr[i].checked){
+            level = levelArr[i].value;
+        }
+        levelArr[i].disabled = true;
+  }
+    msg.textContent = "Guess a number from 1-" + level;
+    answer = Math.floor(Math.random()*level)+1;
+    guess.placeholder = answer;
 }
+
 function makeGuess(){
-
+    let userGuess = parseInt(guess.value);
+    if(isNaN(userGuess) || userGuess < 1 || userGuess > level){
+        msg.textContent = "Enter a VALID #1-" + level;
+        return;
+    }
+    score ++; // valid guess add 1 to score
+    if(userGuess < answer){
+        msg.textContent = "Too low, try again";
+    }
+    else if(userGuess > answer){
+        msg.textContent = "Too high, try again";
+    }
+    else{
+        msg.textContent = "You got it! It took you " + score + " tries. Press play to play again";
+        updateScore();
+        reset();
 }
+}
+
 function reset(){
-
+    guessBtn.disabled = true;
+    guess.disabled = true;
+    guess.value = "";
+    guess.placeholder = "";
+    playBtn.disabled = false;
+    for(let i=0; i<levelArr.length; i++){
+        levelArr[i].disabled = false;
+    }
 }
+
 function updateScore(){
     scoreArr.push(score);
     scoreArr.sort((a,b)=>a-b);
@@ -33,6 +71,7 @@ function updateScore(){
     let avg=sum/scoreArr.length;
     avgScore.textContent="Average Score: "+avg.toFixed(2);
 }
+
 function time(){
     let d=new Date();
     // concatenate a string with all the date info
